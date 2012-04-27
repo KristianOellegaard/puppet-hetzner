518d81571ae6b12c1d15f083f1b0bf0fcaf2c4e3
Kristian Øllegaard <kristian@kristian.io>
2012-04-28 00:16:21 +0200

    Typo

--- lib/facter/hetzer_datacenter.rb
+++ lib/facter/hetzer_datacenter.rb
@@ -1,5 +1,5 @@
 Facter.add("hetzner_datacenter") do
   setcode do
-    Facter::Util::Resolution.exec('traceroute -w 0.5 hetzner.de | grep -o -e 'rz[0-9]*' | tail -n 1')
+    Facter::Util::Resolution.exec("traceroute -w 0.5 hetzner.de | grep -o -e 'rz[0-9]*' | tail -n 1")
   end
 end
