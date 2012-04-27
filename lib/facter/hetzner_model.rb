22719125dc3b484c894a49ac310abf93caedcabe
Kristian Øllegaard <kristian@kristian.io>
2012-04-28 01:12:06 +0200

    Not sure I understand this ruby thing

--- lib/facter/hetzner_model.rb
+++ lib/facter/hetzner_model.rb
@@ -1,13 +1,13 @@
 Facter.add("hetzner_model") do
   setcode do
-    is_virtual? = Facter.value('is_virtual')
-    case is_virtual?
+    is_virtual = Facter.value('is_virtual')
+    case is_virtual
     when false
       cpu = Facter.value('processor0')
-      case cpu
-      when cpu.include? 'E31245'
+      case cpu.include?
+      when 'E31245'
         return "EX 6"
-      when cpu.include? 'E31275'
+      when 'E31275'
         return "EX 8"
       end
     when true
