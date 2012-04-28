4f477fc7ae1a6090d8ebd404a544710aa5386872
Kristian Øllegaard <kristian@kristian.io>
2012-04-28 02:57:47 +0200

    updated model script

--- lib/facter/hetzner_model.rb
+++ lib/facter/hetzner_model.rb
@@ -2,16 +2,16 @@ Facter.add("hetzner_model") do
   setcode do
     is_virtual = Facter.value('is_virtual')
     case is_virtual
-    when false
+    when "false"
       cpu = Facter.value('processor0')
-      case cpu.include?
-      when 'E31245'
-        return "EX 6"
-      when 'E31275'
-        return "EX 8"
+      model = case cpu
+        when /2600/ then "EX 4/4S"
+        when /920/ then "EX 5"
+        when /E31245/ then "EX 6"
+        when /E31275/ then "EX 8"
+        when /3930K/ then "EX 10"
       end
-    when true
-      return false
+      model
     end
   end
 end
